2afa1e9656960f90b9cb7c2fdf7349425a9b445c
zhennann <zhen.nann@icloud.com>
2022-02-15 12:01:09 +0800

    feat: showCurrent when search init

--- static/scripts/collapse.js
+++ static/scripts/collapse.js
@@ -1,14 +1,13 @@
 function showCurrent(){
     //only current page (if it exists) should be opened
     var file = window.location.pathname.split("/").pop().replace(/\.html/, '');
-    console.log('file: ',file);
     document.querySelectorAll("nav > ul > li > a").forEach(function(parent) {
         var href = parent.attributes.href.value.replace(/\.html/, '');
-        console.log('href: ',href);
         if (file === href) {
             parent.parentNode.querySelectorAll("ul li").forEach(function(elem) {
                 elem.style.display = "block";
             });
+            parent.parentNode.style.display="block";
         }
     });
 }
